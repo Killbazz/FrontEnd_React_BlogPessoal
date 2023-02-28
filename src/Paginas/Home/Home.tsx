e3b7524f2b8967f9d564a8071361c94e3d1ba75f
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-02-27 22:48:15 -0300

    Instalado Material UI

---
+++ src/Paginas/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/bCY98Q1.jpeg" alt="Matrix Inicial" className="img"/>
+        </>
+    )
+}
+
+export default Home;
