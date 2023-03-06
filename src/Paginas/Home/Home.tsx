3d44d6eedcd29b0d6d37b0a1864157b4423cc2af
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-06 12:05:04 -0300

    estilização css

--- src/Paginas/Home/Home.tsx
+++ src/Paginas/Home/Home.tsx
@@ -7,22 +7,22 @@ import { Height } from "@material-ui/icons";
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#FF8C00" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Café com Marlon!</Typography> 
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Deixe aqui sua mensagem ou apoio!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className= 'titulo'> Café com Marlon!</Typography> 
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center"  className= 'titulo'> Deixe aqui sua mensagem ou apoio!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#FF0000", color: "white" }}>Ver Mais</Button>
+                        <Button variant="outlined" className='botao'>Ver Mais</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://imgur.com/kuvsOAv.png" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className='postagens'>
                 </Grid>
             </Grid>
         </>
--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,35 +4,36 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography,Grid } from '@material-ui/core';
 import {Box} from '@mui/material';
+import './Footer.css';
 
 function Footer() {
     return (
         <>
             <Grid  container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#FF4500", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Me siga nas redes sociais </Typography>
+                        </Box> 
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                                <FacebookIcon className='redes' />
                             </a>
                             <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className='redes'/>
                             </a>
                             <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className='redes'/>
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2023 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Marlon Bassoto</Typography>
                             </a>
                         </Box>
                     </Box>
