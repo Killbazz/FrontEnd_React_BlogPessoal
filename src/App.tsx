5ea44b5b45121fae7939fa2a6654d08676bdac0a
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-11 09:51:22 -0300

    modificacoes

--- src/App.tsx
+++ src/App.tsx
@@ -18,40 +18,38 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ minHeight: '100vh' }}>
+    
+        <div style={{ minHeight: '100vh' }}>
         <Routes>
+        <Route path="/" element={<Login />} />
 
-          <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
+        <Route path="/home" element={<Home />} />
 
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-          <Route path="/login" element={<Login />} />
+        <Route path="/temas" element={<ListaTema />} />
 
-          <Route path="/home" element={<Home />} />
+        <Route path="/posts" element={<ListaPostagem />} />
 
-          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
 
-          <Route path="/temas" element={<ListaTema />} />
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
 
-          <Route path="/posts" element={<ListaPostagem />} />
+        <Route path="/formularioTema" element={<CadastroTema />} />
 
-          <Route path="/formularioPostagem" element={<CadastroPost />} />
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
 
-          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
 
-          <Route path="/formularioTema" element={<CadastroTema />} />
-
-          <Route path="/formularioTema/:id" element={<CadastroTema />} />
-
-          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
-
-          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
 
         </Routes>
-      </div>
+        </div>
+     
       <Footer />
-
     </Router>
   );
 }
